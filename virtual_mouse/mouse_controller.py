82e61021c43fe18f2215f00de34faa3ae54ba725
Ignaciobrenas <ignaciobrenas@gmail.com>
2026-06-02 18:40:00 +0200

    feat(mouse): add active margin boundaries for effortless corner access

--- virtual_mouse/mouse_controller.py
+++ virtual_mouse/mouse_controller.py
@@ -2,10 +2,13 @@ import pyautogui
 import numpy as np
 
 class MouseController:
-    def __init__(self):
+    def __init__(self, margin=50):
         self.screen_w, self.screen_h = pyautogui.size()
+        self.margin = margin
 
     def move_mouse(self, x, y, cam_w, cam_h):
-        screen_x = np.interp(x, (0, cam_w), (0, self.screen_w))
-        screen_y = np.interp(y, (0, cam_h), (0, self.screen_h))
+        screen_x = np.interp(x, (self.margin, cam_w - self.margin), (0, self.screen_w))
+        screen_y = np.interp(y, (self.margin, cam_h - self.margin), (0, self.screen_h))
+        screen_x = np.clip(screen_x, 0, self.screen_w - 1)
+        screen_y = np.clip(screen_y, 0, self.screen_h - 1)
         pyautogui.moveTo(screen_x, screen_y)
